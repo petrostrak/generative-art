8329546815a022b1dee8ee6bf7b3ca0aa70c198e
petrostrak <pit.trak@gmail.com>
2023-07-26 13:11:26 +0300

    add circumference

--- circle_packing/src/main.rs
+++ circle_packing/src/main.rs
@@ -1,10 +1,10 @@
 use nannou::prelude::*;
 
-// const LINE_WIDTH: f32 = 2.0;
+const LINE_WIDTH: f32 = 2.0;
 const MIN_RADIUS: f32 = 2.0;
-const MAX_RADIUS: f32 = 120.0;
-const N_CIRCLES: usize = 100;
-// const CREATE_CIRCLE_ATTEMPTS: usize = 10;
+const MAX_RADIUS: f32 = 5.0;
+const N_CIRCLES: usize = 1500;
+// const CREATE_CIRCLE_ATTEMPTS: usize = 500;
 
 struct Model;
 
@@ -24,21 +24,25 @@ fn view(app: &App, _model: &Model, frame: Frame) {
     let draw = app.draw();
 
     // set background to blue
-    draw.background().color(BLUE);
+    draw.background().color(WHITE);
 
-    let points = (0..=N_CIRCLES).map(|_| {
+    for _ in 0..=N_CIRCLES {
         let x = random_range(window.left(), window.right());
         let y = random_range(window.top(), window.bottom());
         let radius = random_range(MIN_RADIUS, MAX_RADIUS);
 
-        (pt2(x, y), radius, WHITE)
-    });
+        let line_points = (0..=360).map(|i| {
+            let radian = deg_to_rad(i as f32);
+            let x_ = x + radian.sin() + radius;
+            let y_ = y + radian.cos() + radius;
+            (pt2(x_, y_), BLACK)
+        });
 
-    for (point, radius, color) in points {
-        draw.ellipse()
-            .w_h(radius, radius)
-            .x_y(point.x, point.y)
-            .color(color);
+        draw.ellipse().w_h(radius, radius).x_y(x, y).color(GRAY);
+
+        draw.polyline()
+            .weight(LINE_WIDTH)
+            .points_colored(line_points);
     }
 
     // put everything on the frame
