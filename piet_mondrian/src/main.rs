7ebc8d6610aea0fbb83c364c44c1f1a992799d23
petrostrak <pit.trak@gmail.com>
2023-07-27 11:41:46 +0300

    Refactor code

--- piet_mondrian/src/main.rs
+++ piet_mondrian/src/main.rs
@@ -1,7 +1,7 @@
 use nannou::prelude::*;
 
 const LINE_WIDTH: f32 = 6.0;
-const N_RECTANGLES: usize = 14;
+// const N_RECTANGLES: usize = 14;
 
 struct Model;
 
@@ -11,43 +11,67 @@ fn main() {
 
 fn model(app: &App) -> Model {
     app.set_loop_mode(LoopMode::loop_once());
-    let _window = app.new_window().size(400, 600).view(view).build().unwrap();
+    let _window = app.new_window().size(500, 500).view(view).build().unwrap();
     Model
 }
 
 struct Rectangle {
-    x: f32,
-    y: f32,
-    width: f32,
-    height: f32,
+    side_a: (Vec2, Vec2),
+    side_b: (Vec2, Vec2),
+    side_c: (Vec2, Vec2),
+    side_d: (Vec2, Vec2),
+}
+
+pub struct Rectangles(Vec<Rectangle>);
+impl Rectangles {
+    fn new() -> Vec<Rectangle> {
+        let mut rectangles = Vec::<Rectangle>::new();
+        let rectangle = Rectangle {
+            side_a: (pt2(-247.0, 244.0), pt2(247.0, 244.0)),
+            side_b: (pt2(244.0, 244.0), pt2(244.0, -244.0)),
+            side_c: (pt2(247.0, -244.0), pt2(-247.0, -244.0)),
+            side_d: (pt2(-244.0, -244.0), pt2(-244.0, 244.0)),
+        };
+        rectangles.push(rectangle);
+        rectangles
+    }
 }
 
 fn view(app: &App, _model: &Model, frame: Frame) {
     let window = app.window_rect();
     let draw = app.draw();
-    let size_w = window.w().try_into().map(|f: f32| f - LINE_WIDTH).unwrap();
-    let size_h = window.h().try_into().map(|f: f32| f - LINE_WIDTH).unwrap();
+    // let size_w = window.w().try_into().map(|f: f32| f - LINE_WIDTH).unwrap();
+    // let size_h = window.h().try_into().map(|f: f32| f - LINE_WIDTH).unwrap();
 
-    draw.background().color(BLACK);
-
-    let mut rectangles = Vec::<Rectangle>::with_capacity(N_RECTANGLES);
-    let rectangle = Rectangle {
-        x: 0.0,
-        y: 0.0,
-        width: size_w,
-        height: size_h,
-    };
-    rectangles.push(rectangle);
+    draw.background().color(WHITE);
+    let rectangles = Rectangles::new();
 
     for rectangle in rectangles {
-        draw.rect()
-            .x(rectangle.x)
-            .y(rectangle.y)
-            .height(rectangle.height)
-            .width(rectangle.width)
-            .stroke_weight(LINE_WIDTH)
-            .color(WHITE);
+        draw_rectangle_circumference(&draw, &rectangle);
     }
 
     draw.to_frame(app, &frame).unwrap();
+}
+
+fn draw_rectangle_circumference(draw: &Draw, rectangle: &Rectangle) {
+    draw.line()
+        .start(rectangle.side_a.0)
+        .end(rectangle.side_a.1)
+        .weight(LINE_WIDTH)
+        .color(BLACK);
+    draw.line()
+        .start(rectangle.side_b.0)
+        .end(rectangle.side_b.1)
+        .weight(LINE_WIDTH)
+        .color(BLACK);
+    draw.line()
+        .start(rectangle.side_c.0)
+        .end(rectangle.side_c.1)
+        .weight(LINE_WIDTH)
+        .color(BLACK);
+    draw.line()
+        .start(rectangle.side_d.0)
+        .end(rectangle.side_d.1)
+        .weight(LINE_WIDTH)
+        .color(BLACK);
 }
