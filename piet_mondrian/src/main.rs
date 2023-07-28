0999749469dfb37d14bf538ec9a2fb30c15d9a46
petrostrak <pit.trak@gmail.com>
2023-07-28 12:54:17 +0300

    Refactor piet_mondrian

--- piet_mondrian/src/main.rs
+++ piet_mondrian/src/main.rs
@@ -1,7 +1,6 @@
 use nannou::prelude::*;
 
-const LINE_WIDTH: f32 = 6.0;
-// const N_RECTANGLES: usize = 14;
+const LINE_WIDTH: f32 = 8.0;
 
 struct Model;
 
@@ -16,62 +15,35 @@ fn model(app: &App) -> Model {
 }
 
 struct Rectangle {
-    side_a: (Vec2, Vec2),
-    side_b: (Vec2, Vec2),
-    side_c: (Vec2, Vec2),
-    side_d: (Vec2, Vec2),
-}
-
-pub struct Rectangles(Vec<Rectangle>);
-impl Rectangles {
-    fn new() -> Vec<Rectangle> {
-        let mut rectangles = Vec::<Rectangle>::new();
-        let rectangle = Rectangle {
-            side_a: (pt2(-247.0, 244.0), pt2(247.0, 244.0)),
-            side_b: (pt2(244.0, 244.0), pt2(244.0, -244.0)),
-            side_c: (pt2(247.0, -244.0), pt2(-247.0, -244.0)),
-            side_d: (pt2(-244.0, -244.0), pt2(-244.0, 244.0)),
-        };
-        rectangles.push(rectangle);
-        rectangles
-    }
+    x: f32,
+    y: f32,
+    width: f32,
+    height: f32,
 }
 
 fn view(app: &App, _model: &Model, frame: Frame) {
     let window = app.window_rect();
     let draw = app.draw();
-    // let size_w = window.w().try_into().map(|f: f32| f - LINE_WIDTH).unwrap();
-    // let size_h = window.h().try_into().map(|f: f32| f - LINE_WIDTH).unwrap();
 
     draw.background().color(WHITE);
-    let rectangles = Rectangles::new();
+
+    let mut rectangles = Vec::<Rectangle>::new();
+    let rect = Rectangle {
+        x: 0.0,
+        y: 0.0,
+        width: window.len() - LINE_WIDTH,
+        height: window.len() - LINE_WIDTH,
+    };
+    rectangles.push(rect);
 
     for rectangle in rectangles {
-        draw_rectangle_circumference(&draw, &rectangle);
+        draw.rect()
+            .no_fill()
+            .x_y(rectangle.x, rectangle.y)
+            .w_h(rectangle.width, rectangle.height)
+            .stroke_weight(LINE_WIDTH)
+            .stroke(BLACK);
     }
 
     draw.to_frame(app, &frame).unwrap();
-}
-
-fn draw_rectangle_circumference(draw: &Draw, rectangle: &Rectangle) {
-    draw.line()
-        .start(rectangle.side_a.0)
-        .end(rectangle.side_a.1)
-        .weight(LINE_WIDTH)
-        .color(BLACK);
-    draw.line()
-        .start(rectangle.side_b.0)
-        .end(rectangle.side_b.1)
-        .weight(LINE_WIDTH)
-        .color(BLACK);
-    draw.line()
-        .start(rectangle.side_c.0)
-        .end(rectangle.side_c.1)
-        .weight(LINE_WIDTH)
-        .color(BLACK);
-    draw.line()
-        .start(rectangle.side_d.0)
-        .end(rectangle.side_d.1)
-        .weight(LINE_WIDTH)
-        .color(BLACK);
 }
