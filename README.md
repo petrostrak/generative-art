5b70c1f24f76281a91d4b16c40c26c40246a7818
petrostrak <pit.trak@gmail.com>
2023-08-03 11:51:47 +0300

    Add julia fractal

--- README.md
+++ README.md
@@ -7,3 +7,4 @@ Exploring ideas and techniques used in generative art written in Rust.
 ![alt text](https://github.com/petrostrak/generative-art/blob/main/tiled_lines.png)
 ![alt text](https://github.com/petrostrak/generative-art/blob/main/piet_mondrian.svg)
 ![alt text](https://github.com/petrostrak/generative-art/blob/main/mandelbrot.png)
+![alt text](https://github.com/petrostrak/generative-art/blob/main/julia_fractal.png)
---
+++ julia_fractal/src/main.rs
@@ -0,0 +1,38 @@
+use image::{ImageBuffer, Rgb};
+use nalgebra::Complex;
+
+fn julian(c: Complex<f64>, x: f64, y: f64) -> u8 {
+    let mut z = Complex::new(x, y);
+
+    for i in 0..255 {
+        if z.norm() > 2.0 {
+            return i as u8;
+        }
+
+        z = z * z + c;
+    }
+
+    255
+}
+
+fn main() {
+    let width = 800;
+    let height = 600;
+
+    let scale_x = 3.0 / width as f64;
+    let scale_y = 3.0 / height as f64;
+
+    let mut img = ImageBuffer::new(width, height);
+
+    for (x, y, pixel) in img.enumerate_pixels_mut() {
+        let cx = x as f64 * scale_x - 1.5;
+        let cy = y as f64 * scale_y - 1.5;
+
+        let c = Complex::new(-0.8, 0.156);
+        let value = julian(c, cx, cy);
+
+        *pixel = Rgb([value, value, value]);
+    }
+
+    img.save("julia_fractal.png");
+}
