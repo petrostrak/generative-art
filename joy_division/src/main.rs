6352a1cdf62196aec0e6358104e179d6546de9a5
petrostrak <pit.trak@gmail.com>
2023-08-05 11:08:30 +0300

    explicit vec declaration

--- joy_division/src/main.rs
+++ joy_division/src/main.rs
@@ -28,9 +28,9 @@ fn view(app: &App, _model: &Model, frame: Frame) {
     let draw = app.draw().x_y(-window.w() * 0.5, -window.h() * 0.5);
     draw.background().color(SNOW);
 
-    let mut lines = vec![];
+    let mut lines = Vec::<Vec<Vec2>>::new();
     for i in (STEP..=SIZE - STEP).step_by(STEP) {
-        let mut line = vec![];
+        let mut line = Vec::<Vec2>::new();
         for j in (STEP..=SIZE - STEP).step_by(STEP) {
             let distance_to_center = (j as f32 - SIZE as f32 / 2.0).abs();
             let variance = (SIZE as f32 / 2.0 - 50.0 - distance_to_center).max(0.0);
@@ -54,6 +54,22 @@ fn view(app: &App, _model: &Model, frame: Frame) {
 
         draw.polyline().weight(LINE_WIDTH).points_colored(vertices);
     }
+
+    // Attempt to implement bezier curve formula
+    // for i in 0..lines.len() - 5 {
+    //     for j in 0..lines[i].len() - 1 {
+    //         let xc = (lines[i][j].x + lines[i][j + 1].x) / 2.0;
+    //         let yc = (lines[i][j].y + lines[i][j + 1].y) / 2.0;
+
+    //         draw.path().stroke().weight(LINE_WIDTH).color(BLACK).events(
+    //             nannou::geom::Path::builder()
+    //                 .begin(pt2(lines[i][0].x, lines[i][0].y))
+    //                 .quadratic_bezier_to(pt2(lines[i][j].x, lines[i][j].y), pt2(xc, yc))
+    //                 .build()
+    //                 .iter(),
+    //         );
+    //     }
+    // }
 
     draw.to_frame(app, &frame).unwrap();
 }
