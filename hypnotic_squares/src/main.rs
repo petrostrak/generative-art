92171e8e5cb7b92680699e8b2ce72ec5bf2f1261
petrostrak <pit.trak@gmail.com>
2023-08-05 13:30:58 +0300

    implement hypnotic squares

--- hypnotic_squares/src/main.rs
+++ hypnotic_squares/src/main.rs
@@ -1,14 +1,14 @@
 use nannou::prelude::*;
 
-const ROWS: u32 = 7;
-const COLS: u32 = 7;
 const SIZE: u32 = 320;
 const LINE_WIDTH: f32 = 1.2;
-const WIDTH: u32 = 320;
-const HEIGHT: u32 = 320;
+const WIDTH: u32 = SIZE;
+const HEIGHT: u32 = SIZE;
 const FINAL_SIZE: f32 = 10.0;
-const START_SIZE: f32 = SIZE as f32;
-const START_STEP: f32 = 5.0;
+const START_STEPS: f32 = 4.0;
+const OFFSET: f32 = 2.0;
+const TILE_STEP: f32 = (SIZE as f32 - OFFSET * 2.0) / 8.0;
+const START_SIZE: f32 = TILE_STEP;
 
 fn main() {
     nannou::sketch(view)
@@ -18,27 +18,33 @@ fn main() {
 }
 
 fn view(app: &App, frame: Frame) {
-    // let window = app.window_rect();
-    let draw = app.draw();
+    let window = app.window_rect();
+    let draw = app.draw().x_y(-window.w() * 0.45, -window.h() * 0.45);
 
     draw.background().color(SNOW);
 
-    // for y in 0..ROWS {
-    //     for x in 0..COLS {
-    //         let cdraw = gdraw.x_y(x as f32, y as f32);
+    let directions = vec![-1, 0, 1];
+    let mut start_steps: f32;
 
-    //         cdraw
-    //             .rect()
-    //             .no_fill()
-    //             .stroke(BLACK)
-    //             .stroke_weight(LINE_WIDTH)
-    //             .w_h(1.0, 1.0);
-    //     }
-    // }
-
-    draw_squares(
-        &draw, 0.0, 0.0, START_SIZE, START_SIZE, 0.0, 0.0, START_STEP,
-    );
+    for x in (0..=SIZE).step_by(47) {
+        for y in (0..=SIZE).step_by(47) {
+            start_steps = 2.0 + f32::ceil(random_f32() * 3.0);
+            let x_direction =
+                directions[f32::floor(random_f32() * directions.len() as f32) as usize];
+            let y_direction =
+                directions[f32::floor(random_f32() * directions.len() as f32) as usize];
+            draw_squares(
+                &draw,
+                x as f32,
+                y as f32,
+                START_SIZE,
+                START_SIZE,
+                x_direction as f32,
+                y_direction as f32,
+                start_steps - 1.0,
+            )
+        }
+    }
 
     draw.to_frame(app, &frame).unwrap();
 }
@@ -57,13 +63,13 @@ fn draw_squares(
         .no_fill()
         .stroke(BLACK)
         .stroke_weight(LINE_WIDTH)
-        // .x_y(x, y)
+        .x_y(x, y)
         .w_h(width, height);
 
     if steps >= 0.0 {
-        let new_size = START_SIZE * (steps / START_STEP) + FINAL_SIZE;
-        let mut new_x: f32 = x + (width - new_size) / 2.0;
-        let mut new_y = y + (height - new_size) / 2.0;
+        let new_size = START_SIZE * (steps / START_STEPS) + FINAL_SIZE;
+        let mut new_x: f32 = x + (width - new_size) / 4.0;
+        let mut new_y: f32 = y + (height - new_size) / 4.0;
         new_x = new_x - ((x - new_x) / (steps + 2.0)) * x_movement;
         new_y = new_y - ((y - new_y) / (steps + 2.0)) * y_movement;
         draw_squares(
